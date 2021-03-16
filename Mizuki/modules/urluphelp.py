50b1155cffd09b5f8f68b655ee5ab56e154907fd
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-03-16 17:37:44 +0530

    Update urluphelp.py

--- Mizuki/modules/urluphelp.py
+++ Mizuki/modules/urluphelp.py
@@ -1,6 +1,6 @@
 __help__ = """
- • `/up`*:* reply to a direct download link to upload it to telegram as files
+ • /up*:* reply to a direct download link to upload it to telegram as files
 
-© @TheMizukiBot 👸"""
+© @Widow_robot 👸"""
 
 __mod_name__ = "URL Upload 📤"
